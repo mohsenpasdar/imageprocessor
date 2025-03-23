340953b906f0edd65da960703d0744952456dc35
mohsen <mohsenput2@gmail.com>
2025-03-23 14:35:44 -0400

    Rotate for all of the pixels: Rotate is done!

---
+++ src/Rotate.java
@@ -0,0 +1,18 @@
+import java.awt.image.BufferedImage;
+
+public class Rotate extends Converter {
+    @Override
+    public BufferedImage specificConvert(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        BufferedImage newImage = new BufferedImage(height, width, BufferedImage.TYPE_INT_ARGB);
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int pixelInt = image.getRGB(i, j);
+                newImage.setRGB(height - j - 1, i, pixelInt);
+            }
+        }
+        return newImage;
+    }
+}
