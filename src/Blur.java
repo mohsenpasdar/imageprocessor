c22f55befb2fc916df558faa4abe3ec818cc2093
mohsen <mohsenput2@gmail.com>
2025-03-23 17:43:16 -0400

    Blur- improving the method in the Blur class, to reduce the runtime

--- src/Blur.java
+++ src/Blur.java
@@ -9,64 +9,28 @@ public class Blur extends Converter {
 
         for (int i = 1; i < width - 1; i++) {
             for (int j = 1; j < height - 1; j++) {
-                int alpha = (
-                          pixelToARGB(image, i - 1, j - 1).alpha
-                        + pixelToARGB(image, i - 1, j).alpha
-                        + pixelToARGB(image, i - 1, j + 1).alpha
-                        + pixelToARGB(image, i, j - 1).alpha
-                        + pixelToARGB(image, i, j).alpha
-                        + pixelToARGB(image, i, j + 1).alpha
-                        + pixelToARGB(image, i + 1, j - 1).alpha
-                        + pixelToARGB(image, i + 1, j).alpha
-                        + pixelToARGB(image, i + 1, j + 1).alpha
-                ) / 9;
-
-                int red = (
-                          pixelToARGB(image, i - 1, j - 1).red
-                        + pixelToARGB(image, i - 1, j).red
-                        + pixelToARGB(image, i - 1, j + 1).red
-                        + pixelToARGB(image, i, j - 1).red
-                        + pixelToARGB(image, i, j).red
-                        + pixelToARGB(image, i, j + 1).red
-                        + pixelToARGB(image, i + 1, j - 1).red
-                        + pixelToARGB(image, i + 1, j).red
-                        + pixelToARGB(image, i + 1, j + 1).red
-                ) / 9;
-
-                int green = (
-                        pixelToARGB(image, i - 1, j - 1).green
-                                + pixelToARGB(image, i - 1, j).green
-                                + pixelToARGB(image, i - 1, j + 1).green
-                                + pixelToARGB(image, i, j - 1).green
-                                + pixelToARGB(image, i, j).green
-                                + pixelToARGB(image, i, j + 1).green
-                                + pixelToARGB(image, i + 1, j - 1).green
-                                + pixelToARGB(image, i + 1, j).green
-                                + pixelToARGB(image, i + 1, j + 1).green
-                ) / 9;
-
-                int blue = (
-                          pixelToARGB(image, i - 1, j - 1).blue
-                        + pixelToARGB(image, i - 1, j).blue
-                        + pixelToARGB(image, i - 1, j + 1).blue
-                        + pixelToARGB(image, i, j - 1).blue
-                        + pixelToARGB(image, i, j).blue
-                        + pixelToARGB(image, i, j + 1).blue
-                        + pixelToARGB(image, i + 1, j - 1).blue
-                        + pixelToARGB(image, i + 1, j).blue
-                        + pixelToARGB(image, i + 1, j + 1).blue
-                ) / 9;
-
-                ARGB newPixelARGB = new ARGB(alpha, red, green, blue);
-                int newPixelInt = newPixelARGB.toInt();
-                newImage.setRGB(i, j, newPixelInt);
+                newImage.setRGB(i, j, averagePixelInt(image, i, j));
             }
         }
         return newImage;
     }
 
-    protected ARGB pixelToARGB(BufferedImage image, int x, int y) {
-        int pixelInt = image.getRGB(x, y);
-        return new ARGB(pixelInt);
+    protected int averagePixelInt(BufferedImage image, int x, int y) {
+        int sumAlpha = 0;
+        int sumRed = 0;
+        int sumGreen = 0;
+        int sumBlue = 0;
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1 ; j++) {
+                int pixelInt = image.getRGB(i, j);
+                ARGB pixelARGB = new ARGB(pixelInt);
+                sumAlpha += pixelARGB.alpha;
+                sumRed += pixelARGB.red;
+                sumGreen += pixelARGB.green;
+                sumBlue += pixelARGB.blue;
+            }
+        }
+        ARGB newPixelARGB = new ARGB(sumAlpha / 9, sumRed / 9, sumGreen / 9, sumBlue / 9);
+        return newPixelARGB.toInt();
     }
 }
