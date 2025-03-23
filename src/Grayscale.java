299d91cf95cf885b15255f5359da6c9e5b879762
mohsen <mohsenput2@gmail.com>
2025-03-23 12:23:46 -0400

    doing the grayscale for all the pixels: GrayScale is done!

--- src/Grayscale.java
+++ src/Grayscale.java
@@ -2,23 +2,23 @@ import java.awt.image.BufferedImage;
 
 public class Grayscale extends Converter {
     @Override
-    public BufferedImage specificConvert(BufferedImage original) {
-        int width = original.getWidth();
-        int height = original.getHeight();
-        int pixel = original.getRGB(151, 102);
-        ARGB pixelARGB = new ARGB(pixel);
-        int alpha = pixelARGB.alpha;
-        int red = pixelARGB.red;
-        int green = pixelARGB.green;
-        int blue = pixelARGB.blue;
-        System.out.println("Alpha: " + pixelARGB.alpha + ", Red: " + pixelARGB.red + ", Green: " + pixelARGB.green + ", Blue: " + pixelARGB.blue);
-        int gray = (red + green + blue) / 3;
-        pixelARGB = new ARGB(alpha, gray, gray, gray);
-        pixel = pixelARGB.toInt();
-        original.setRGB(151, 102, pixel);
-        int newPixel = original.getRGB(151, 102);
-        ARGB newColor = new ARGB(newPixel);
-        System.out.println("Alpha: " + newColor.alpha + ", Red: " + newColor.red + ", Green: " + newColor.green + ", Blue: " + newColor.blue);
-        return original;
+    public BufferedImage specificConvert(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int pixelInt = image.getRGB(i, j);
+                ARGB pixelARGB = new ARGB(pixelInt);
+                int alpha = pixelARGB.alpha;
+                int red = pixelARGB.red;
+                int green = pixelARGB.green;
+                int blue = pixelARGB.blue;
+                int gray = (red + green + blue) / 3;
+                pixelARGB = new ARGB(alpha, gray, gray, gray);
+                pixelInt = pixelARGB.toInt();
+                image.setRGB(i, j, pixelInt);
+            }
+        }
+        return image;
     }
 }
