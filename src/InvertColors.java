7c5dddb900e78a18485b3b17e8700d529bb2d9b2
mohsen <mohsenput2@gmail.com>
2025-03-29 20:57:51 -0400

    InvertColors: Inverting each pixel's color by (r/g/b = 255 - r/g/b)- removing prev implementation which was by loop, and adding new implementation which is by recursion

---
+++ src/InvertColors.java
@@ -0,0 +1,41 @@
+import java.awt.image.BufferedImage;
+
+public class InvertColors extends Converter {
+    @Override
+    public BufferedImage specificConvert(BufferedImage image) {
+        int row = 0;
+        helperInvertAllRows(row, image);
+        return image;
+    }
+
+    private void helperInvertAllRows(int row, BufferedImage image) {
+        if (row == image.getHeight()) {
+            return;
+        }
+        int col = 0;
+        helperInvertRow(col, row, image);
+        row++;
+        helperInvertAllRows(row, image);
+    }
+
+    private void helperInvertRow(int col, int row, BufferedImage image) {
+        if (col == image.getWidth()) {
+            return;
+        }
+        invertPixel(col, row, image);
+        col++;
+        helperInvertRow(col, row, image);
+    }
+
+    private void invertPixel(int col, int row, BufferedImage image) {
+        int pixelInt = image.getRGB(col, row);
+        ARGB pixelARGB = new ARGB(pixelInt);
+        int alpha = pixelARGB.alpha;
+        int red = 255 - pixelARGB.red;
+        int green = 255 - pixelARGB.green;
+        int blue = 255 - pixelARGB.blue;
+        ARGB newPixelARGB = new ARGB(alpha, red, green, blue);
+        int newPixelARGBInt = newPixelARGB.toInt();
+        image.setRGB(col, row, newPixelARGBInt);
+    }
+}
