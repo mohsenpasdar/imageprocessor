aa4e3df531b34c906f55f27b0ad7e09c26980b0b
mohsen <mohsenput2@gmail.com>
2025-04-12 10:55:46 -0400

    Change the code for FlipVertical from recursion to loop

--- src/FlipVertical.java
+++ src/FlipVertical.java
@@ -4,29 +4,34 @@ public class FlipVertical extends Converter {
 
     @Override
     public BufferedImage specificConvert(BufferedImage image) {
-        int i = 0;
-        helperFlipAllColumns(i, image);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height / 2; j++) {
+                swapColumnPixels(i, j, image);
+            }
+        }
         return image;
     }
 
-    private void helperFlipAllColumns(int column, BufferedImage image) {
-        if (column == image.getWidth()) {
-            return;
-        }
-        int row = 0;
-        helperFlipColumn(column, row, image);
-        column++;
-        helperFlipAllColumns(column, image);
-    }
-
-    private void helperFlipColumn(int column, int row, BufferedImage image) {
-        if (row == image.getHeight() / 2) {
-            return;
-        }
-        swapColumnPixels(column, row, image);
-        row++;
-        helperFlipColumn(column, row, image);
-    }
+//    private void helperFlipAllColumns(int column, BufferedImage image) {
+//        if (column == image.getWidth()) {
+//            return;
+//        }
+//        int row = 0;
+//        helperFlipColumn(column, row, image);
+//        column++;
+//        helperFlipAllColumns(column, image);
+//    }
+//
+//    private void helperFlipColumn(int column, int row, BufferedImage image) {
+//        if (row == image.getHeight() / 2) {
+//            return;
+//        }
+//        swapColumnPixels(column, row, image);
+//        row++;
+//        helperFlipColumn(column, row, image);
+//    }
 
     private void swapColumnPixels(int column, int row, BufferedImage image) {
         int height = image.getHeight();
