5c2771dddbcb39744bfb5a5f5d175f045a068c55
mohsen <mohsenput2@gmail.com>
2025-04-26 09:27:14 -0400

    Introduce a static blick_size var to the class, and changes the methods to reflect this; add some comments to Pixelate class to enhance readability

--- src/Pixelate.java
+++ src/Pixelate.java
@@ -1,14 +1,29 @@
 import java.awt.image.BufferedImage;
 
+/**
+ * Pixelate converter that creates a pixelated (blocky) effect by dividing the image into blocks (3 * 3)
+ * and setting each block's pixels to the average color of that block.
+ */
 public class Pixelate extends Converter {
+
+    // Size of each pixelated block (can easily be changed)
+    private static final int BLOCK_SIZE = 3;
+
+    /**
+     * Apply a pixelation effect to the input image.
+     *
+     * @param image the original BufferedImage
+     * @return a new BufferedImage with the pixelated effect
+     */
     @Override
     public BufferedImage process(BufferedImage image) {
         int width = image.getWidth();
         int height = image.getHeight();
         BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
-        for (int i = 0; i < width; i = i + 3) {
-            for (int j = 0; j < height; j = j + 3) {
+        // Loop through image in blocks of BLOCK_SIZE
+        for (int i = 0; i < width; i = i + BLOCK_SIZE) {
+            for (int j = 0; j < height; j = j + BLOCK_SIZE) {
                 int blockAvgColor = avgBlock(i, j, image);
                 colorBlock(i, j, newImage, blockAvgColor);
             }
@@ -17,6 +32,14 @@ public class Pixelate extends Converter {
         return newImage;
     }
 
+    /**
+     * Calculate the average color of a BLOCK_SIZE x BLOCK_SIZE block.
+     *
+     * @param x starting x-coordinate of the block
+     * @param y starting y-coordinate of the block
+     * @param image the original BufferedImage
+     * @return the averaged pixel color as an integer
+     */
     private int avgBlock(int x, int y, BufferedImage image) {
         int width = image.getWidth();
         int height = image.getHeight();
@@ -25,8 +48,10 @@ public class Pixelate extends Converter {
         int sumRed = 0;
         int sumGreen = 0;
         int sumBlue = 0;
-        for (int i = x; i <= x + 2; i++) {
-            for (int j = y; j <= y + 2 ; j++) {
+
+        // Calculate the sum of colors within the block
+        for (int i = x; i < x + BLOCK_SIZE; i++) {
+            for (int j = y; j < y + BLOCK_SIZE ; j++) {
                 int col = Math.min(i, width - 1);
                 int row = Math.min(j, height - 1);
                 int pixelInt = image.getRGB(col, row);
@@ -38,16 +63,28 @@ public class Pixelate extends Converter {
             }
         }
 
-        ARGB newPixelARGB = new ARGB(sumAlpha / 9, sumRed / 9, sumGreen / 9, sumBlue / 9);
+        int pixelsCount = BLOCK_SIZE * BLOCK_SIZE;
+
+        // Return the average color of the block
+        ARGB newPixelARGB = new ARGB(sumAlpha / pixelsCount, sumRed / pixelsCount, sumGreen / pixelsCount, sumBlue / pixelsCount);
         return newPixelARGB.toInt();
     }
 
+    /**
+     * Fill a BLOCK_SIZE x BLOCK_SIZE block in the new image with the specified color.
+     *
+     * @param x starting x-coordinate of the block
+     * @param y starting y-coordinate of the block
+     * @param image the new BufferedImage to set pixels
+     * @param pixelInt the color to set for the block
+     */
     private void colorBlock(int x, int y, BufferedImage image, int pixelInt) {
         int width = image.getWidth();
         int height = image.getHeight();
 
-        for (int i = x; i <= x + 2; i++) {
-            for (int j = y; j <= y + 2 ; j++) {
+        // Set each pixel in the block to the average color
+        for (int i = x; i < x + BLOCK_SIZE; i++) {
+            for (int j = y; j < y + BLOCK_SIZE ; j++) {
                 int col = Math.min(i, width - 1);
                 int row = Math.min(j, height - 1);
                 image.setRGB(col, row, pixelInt);
